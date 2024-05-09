8f908e54d9f61530e0c84211a822db82365524ed
Arnav Dandekar <146211212+4rnv@users.noreply.github.com>
2024-05-09 17:01:52 +0000

    "PWA support"

--- static/js/script.js
+++ static/js/script.js
@@ -45,3 +45,17 @@ document.querySelectorAll('.star-rating label').forEach(label => {
         ratingSelect.dispatchEvent(new Event('change'));
     });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('static/js/sw.js').then(registration => {
+            console.log('ServiceWorker registration successful');
+        }, err => {
+            console.log('ServiceWorker registration failed: ', err);
+        }).catch(err => {
+            console.log(err);
+        });
+    });
+  } else {
+    console.log('Service workers are not supported (Firefox Private Browsing).');
+  }
